d759768adb37b223a578bbb802ddcd3af5e92e04
Kota Yatagai <kota@yatagai.com>
2020-09-17 16:48:17 +0900

    Fix integrity of GOogle Fonts

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,4 @@
   import Home from './routes/Home.svelte';
 </script>
 
-<svelte:head>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
-    integrity="sha384-3HFn0PvfcoXb8Iy7LxoZoz+iqEWKnYPogIiNMip2W2NrdArLrcVG+XAW1ANqjj0Z"
-    crossorigin="anonymous"
-    rel="stylesheet" />
-</svelte:head>
-
 <Home />
