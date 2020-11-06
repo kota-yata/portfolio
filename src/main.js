e1517d40c6a11650dfa6563f904c5de7aed6d1a7
Kota Yatagai <kota@yatagai.com>
2020-11-07 02:29:37 +0900

    Fix main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import App from './App.svelte';
 import 'swiper/swiper-bundle.css';
 
-const app = new App({
-  target: document.body,
-  props: {}
-});
+const app = new App({ target: document.body });
 
 export default app;
