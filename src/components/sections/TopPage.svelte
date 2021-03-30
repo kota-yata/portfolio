90d48e44179178ce94271eb8c6e3167c7c94bdd0
kota-yata <kota@yatagai.com>
2021-03-31 04:59:19 +0900

    change top

--- src/components/sections/TopPage.svelte
+++ src/components/sections/TopPage.svelte
@@ -11,8 +11,11 @@
 
 {#if isVisible}
   <div class="top">
-    <div class="top-name" transition:fade={{ delay: 1500, duration: 1500 }}>
-      <h1>Kota Yatagai</h1>
+    <div class="top-inner">
+      <div class="top-inner-name" transition:fade={{ delay: 1500, duration: 1500 }}>
+        <h1>Kota Yatagai</h1>
+        <span class="top-inner-name-desc">High school student && Front-end Developer</span>
+      </div>
     </div>
   </div>
 {/if}
@@ -26,25 +29,37 @@
     background-image: url('../img/bg.webp');
     background-size: cover;
     position: relative;
-    &-name {
-      position: absolute;
-      top: 20px;
-      left: 20px;
-      h1 {
-        color: $light-white;
-        text-align: right;
-        opacity: 0.5;
-        font-size: 30px;
-        font-family: 'Poppins', Arial, Helvetica, sans-serif;
-        margin: 0;
+    &-inner {
+      width: 100%;
+      height: 100%;
+      backdrop-filter: blur(20px);
+      &-name {
+        @extend %center;
+
+        padding-top: 20vh;
+        h1 {
+          color: $light-white;
+          opacity: 0.5;
+          font-size: 100px;
+          font-family: 'Poppins', Arial, Helvetica, sans-serif;
+        }
+        &-desc {
+          color: $black;
+          opacity: 0.8;
+          font-size: 30px;
+          font-family: 'Poppins', Arial, Helvetica, sans-serif;
+        }
       }
     }
   }
 
   @media screen and (max-width: 1000px) {
     .top {
-      &-name {
+      &-inner-name {
         h1 {
+          font-size: 30px;
+        }
+        &-desc {
           font-size: 15px;
         }
       }
