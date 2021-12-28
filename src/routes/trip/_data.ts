a9902e5e7a2c0918d01d2ecaba2685f3994fa359
kota-yata <kota@yatagai.com>
2021-12-27 22:53:10 -0600

    Fixed terrible english

--- src/routes/trip/_data.ts
+++ src/routes/trip/_data.ts
@@ -3,9 +3,9 @@ export const thumbnails: thumbnails[] = [
     location: 'Universal Studio Japan',
     short: 'usj',
     date: 'August 2021',
-    description: `The last trip before studying abroad. \
-    Though I and my friends had already been to Universal Studio Hollywood, we had so much fun the original rides in Osaka. \
-    I was exhausted when we leave there because of motion sickness, but was also satisfied with that we could make a great memory.`,
+    description: `The last trip before leaving Japan. \
+    Universal Studio Japan had a lot of unique rides where you wouldn't find in the other Universal Studios.\
+    Those and a midnight party with my friends were priceless and unforgettable.`,
     image: 9
   },
   {
@@ -13,19 +13,19 @@ export const thumbnails: thumbnails[] = [
     short: 'izu',
     date: 'July 2021',
     description: `Traveled to Izu, a summer resort in Japan with my family. \
-    I tried surfing for the first time, and it was really fun. Since typhoon was coming close, \
-    the sea raged and swimming was banned few days after we got back home. \
-    But the weather was great while we stay.`,
+    I have tried surfing for the first time there, which was awesome. Since typhoon was coming close, \
+    the sea raged and swimming was actually banned a few days after we got back home. \
+    But the weather was wonderful while we stay.`,
     image: 8
   },
   {
     location: 'Los Angels',
     short: 'la',
     date: 'February 2020',
-    description: `The last trip before quarantine. \
-    Traveled around Redondo Beach, Citadel, Beverly Hills, Santa Monica, Hollywood, San Diego with my friends as a school trip. \
-    Also we visited UCLA. The locker room of UCLA basketball team was like that of NBA. \
-    This is definitely the best trip of my life, and I frequently look back this nostalgic, good old days.`,
+    description: `Traveled around Redondo Beach, Citadel, \
+    Beverly Hills, Santa Monica, Hollywood, San Diego with my friends as a school trip. \
+    Also we have visited UCLA. The locker room of UCLA basketball team was just like that of NBA. \
+    This was definitely the best trip of my life, and I frequently look back this nostalgic good old days.`,
     image: 32
   },
   {
@@ -33,7 +33,7 @@ export const thumbnails: thumbnails[] = [
     short: 'bali',
     date: 'July 2018',
     description: `Stayed at RIMBA Jimbaran, a great resort hotel with tons of pool and restaurant, with my family. \
-    We've enjoyed the beauty of the beaches, and deliciousness of the local foods for 4 days. Nothing can spare this wonderful time.`,
+    We have enjoyed the beauty of the beaches, and deliciousness of the local foods for four days. Nothing can spare this wonderful time.`,
     image: 12
   },
 ];
