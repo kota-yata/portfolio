c86e41df7079adee58e05d3d8d761ccfa77af9cd
kota-yata <kota@yatagai.com>
2022-03-14 01:55:50 -0500

    remove header from rss.xml

--- src/routes/rss.xml.ts
+++ src/routes/rss.xml.ts
@@ -1,6 +1,7 @@
 import { getPosts } from '../lib/posts/getPosts';
 
-const xml = (posts: postMeta[]) => `<rss xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom" version="2.0">
+const xml = (posts: postMeta[]) => `<?xml version="1.0" encoding="UTF-8" ?>
+<rss xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom" version="2.0">
 <channel>
   <title>Kota Yatagai</title>
   <link>https://kota-yata.com</link>
@@ -23,14 +24,11 @@ const xml = (posts: postMeta[]) => `<rss xmlns:dc="https://purl.org/dc/elements/
 </rss>`;
 
 export const get = () => {
-  const headers = {
-    'Cache-Control': 'max-age=0, s-maxage=3600',
-    'Content-Type': 'application/xml',
-  };
+  // const headers = {
+  //   'Cache-Control': 'max-age=0, s-maxage=3600',
+  //   'Content-Type': 'application/xml',
+  // };
   const posts = getPosts();
   const body = xml(posts);
-  return {
-    headers,
-    body
-  };
+  return { body };
 };
