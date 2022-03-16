95b45133bd117f6621efdca665ade68e0a73faa6
kota-yata <kota@yatagai.com>
2022-03-16 03:50:07 -0500

    add dummy rss.svelte

---
+++ src/routes/rss.svelte
@@ -0,0 +1,17 @@
+<script context="module" lang="ts">
+  import { DOMParser } from 'xmldom';
+
+  export const load = async ({ fetch }): Promise<{ props: { xml: Document } }> => {
+    const res = await fetch('/rss.xml');
+    const text = res.text();
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(text, "application/xml");
+    return { props: { xml } };
+  };
+</script>
+
+<script lang="ts">
+  export let xml: Document;
+</script>
+
+xml
