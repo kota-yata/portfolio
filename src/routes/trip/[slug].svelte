cfa15055d53a2115d116afbbe14cd7432d982025
kota-yata <kota@yatagai.com>
2022-05-24 17:55:16 -0500

    change viewport threshold

--- src/routes/trip/[slug].svelte
+++ src/routes/trip/[slug].svelte
@@ -23,7 +23,7 @@
   <div class="images">
     {#each Array(found.image) as _, index}
     <picture>
-      <source srcset={`/trip/${found.short}/optimized-mobile/${index}.webp`} media="(max-width: 400px)" type="image/webp">
+      <source srcset={`/trip/${found.short}/optimized-mobile/${index}.webp`} media="(max-width: 500px)" type="image/webp">
       <source srcset={`/trip/${found.short}/optimized/${index}.webp`} type="image/webp">
         <img
         loading="lazy"
