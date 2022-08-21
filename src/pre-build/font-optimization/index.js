f2bfdd01eee1bfd9045fa588d15a08dcad211add
Kota Yatagai <kota@yatagai.com>
2022-08-21 21:04:14 +0900

    remove jp fonts as a test (#33)

---
+++ src/pre-build/font-optimization/index.js
@@ -0,0 +1,14 @@
+
+import { readFileSync } from 'fs';
+
+// const collectChars = () => {
+
+// };
+
+// export const run = (path) => {
+//   const fontBuffer = readFileSync(path);
+//   const xml = decompile(fontBuffer);
+//   console.log(xml);
+// };
+
+// run('../../../static/fonts/-Sans-JP/regular.otf');
